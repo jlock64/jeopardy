e1df9e90936c920dd2f3a9716dc9f44f4eb8f845
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-03-27 22:17:33 -0400

    data is showing up in all columns and rows but still not showing any questions or answers

--- app.services.js
+++ app.services.js
@@ -1,17 +1,51 @@
 angular
   .module('jeopardy')
-  .service('JeopardyService', function($http) {
-    // var key = 'iyhmf98ikk1p52incedpu8n2';
-    var cors = 'https://free-cors-server.herokuapp.com/any-request/';
-    id = Math.random() * 18416;
-    var url = encodeURIComponent('http://jservice.io/api/category?id=' + id);
+  .service('JeopardyService', function($http, $q) {
 
-    // var cacheEngine = $cacheFactory('myCacheEngine');
-       function getJeps() {
-         return $http.get(cors + url);
-       }
+    var url = 'http://jservice.io/api/category?id=';
+    var url2 = 'http://jservice.io/api/categories?count=100&offset='
+
+    function getQuestions() {
+      // var defer = $q.defer();
+      var randomNumber = Math.floor(Math.random() * 18000);
+       return $http.get(url + randomNumber);
+    }
+
+    function getCategories() {
+      // var defer = $q.defer();
+      var randomNumber = Math.floor(Math.random() * 18000);
+       return $http.get(url2 + randomNumber);
+        // .then(function(data) {
+        //   defer.resolve.promise;
+        // });
+      // return defer.promise;
+    }
+
+    function sixCat() {
+      return [getAllCategories(), getAllCategories(), getAllCategories(), getAllCategories(), getAllCategories(), getAllCategories()];
+    }
 
     return {
-      getJeps: getJeps
-    };
-  })
+      getCategories: getCategories,
+      getQuestions: getQuestions,
+      sixCat: sixCat
+    }
+  }) // end of JeopardyService
+
+// angular
+//   .module('jeopardy')
+//   .service('JeopardyService', function($http) {
+//     // var key = 'iyhmf98ikk1p52incedpu8n2';
+//     var cors = 'https://free-cors-server.herokuapp.com/any-request/';
+//     id = Math.floor(Math.random() * 18416);
+//     var url = encodeURIComponent('http://jservice.io/api/category?id=' + id);
+//
+//     // var cacheEngine = $cacheFactory('myCacheEngine');
+//        function getJeps() {
+//          return $http.get(cors + url);
+//        }
+//
+//     return {
+//       getJeps: getJeps,
+//     };
+//   })
--- home.controller.js
+++ home.controller.js
@@ -2,18 +2,18 @@ angular
   .module('jeopardy')
   .controller('HomeController', function($scope, JeopardyService, $routeParams) {
 
-    function pageLoad() {
-      JeopardyService.getJeps()
+
+    function loadQuestions() {
+      JeopardyService.getQuestions()
         .then(function(data) {
-          // console.log(data);
+          console.log("inside questions", data);
           window.glob = data;
           var jeps = data.data.clues.map(function(el) {
-            console.log("inside map", el);
+            // console.log("inside map", el);
             return {
               answer: el.answer,
               question: el.question,
               value: el.value,
-              // category: el.category.title,
               id: el.category_id
             }
           })
@@ -21,9 +21,110 @@ angular
           $scope.title = data.data.title;
           // console.log(jeps);
         })
+    }
+    loadQuestions();
+
+
+    function loadQuestions1() {
+      JeopardyService.getQuestions()
+        .then(function(data) {
+          console.log("inside questions1", data);
+          var jeps1 = data.data.clues.map(function(el) {
+            // console.log("inside map", el);
+            return {
+              answer: el.answer,
+              question: el.question,
+              value: el.value,
+              id: el.category_id
+            }
+          })
+          $scope.jeps1 = jeps1;
+          $scope.title1 = data.data.title;
+        })
 
     }
-    pageLoad();
+    loadQuestions1();
+
+
+    function loadQuestions2() {
+      JeopardyService.getQuestions()
+        .then(function(data) {
+          console.log("inside questions2", data);
+          var jeps2 = data.data.clues.map(function(el) {
+            // console.log("inside map", el);
+            return {
+              answer: el.answer,
+              question: el.question,
+              value: el.value,
+              id: el.category_id
+            }
+          })
+          $scope.jeps2 = jeps2;
+          $scope.title2 = data.data.title;
+          // console.log(jeps);
+        })
+    }
+    loadQuestions2();
+
+    function loadQuestions3() {
+      JeopardyService.getQuestions()
+        .then(function(data) {
+          console.log("inside questions3", data);
+          var jeps3 = data.data.clues.map(function(el) {
+            // console.log("inside map", el);
+            return {
+              answer: el.answer,
+              question: el.question,
+              value: el.value,
+              id: el.category_id
+            }
+          })
+          $scope.jeps3 = jeps3;
+          $scope.title3 = data.data.title;
+          // console.log(jeps);
+        })
+    }
+    loadQuestions3();
+
+    function loadQuestions4() {
+      JeopardyService.getQuestions()
+        .then(function(data) {
+          console.log("inside questions4", data);
+          var jeps4 = data.data.clues.map(function(el) {
+            // console.log("inside map", el);
+            return {
+              answer: el.answer,
+              question: el.question,
+              value: el.value,
+              id: el.category_id
+            }
+          })
+          $scope.jeps4 = jeps4;
+          $scope.title4 = data.data.title;
+          // console.log(jeps);
+        })
+    }
+    loadQuestions4();
+
+    function loadQuestions5() {
+      JeopardyService.getQuestions()
+        .then(function(data) {
+          console.log("inside questions5", data);
+          var jeps5 = data.data.clues.map(function(el) {
+            // console.log("inside map", el);
+            return {
+              answer: el.answer,
+              question: el.question,
+              value: el.value,
+              id: el.category_id
+            }
+          })
+          $scope.jeps5 = jeps5;
+          $scope.title5 = data.data.title;
+          // console.log(jeps);
+        })
+    }
+    loadQuestions5();
 
 
 
